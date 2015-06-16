7b998f9555741ee9d64fd82851e77647e57115d1
Rodrigo Navarro <rnavarro1@gmail.com>
2015-06-16 00:27:08 -0300

    Bootstraping Clojure solutions.

---
+++ clojure/project.clj
@@ -0,0 +1,7 @@
+(defproject euler "0.1.0-SNAPSHOT"
+  :description "Project Euler solutions"
+  :url "https://github.com/reu/project-euler"
+  :license {:name "MIT License"
+            :url "http://www.opensource.org/licenses/mit-license.php"}
+  :main ^:skip-aot euler.core
+  :dependencies [[org.clojure/clojure "1.6.0"]])
---
+++ clojure/src/euler/core.clj
@@ -0,0 +1,16 @@
+(ns euler.core)
+
+(use 'clojure.java.io)
+
+(defn run-problem
+  [number]
+  (def file-name (str "src/euler/problem_" (format "%03d" number) ".clj"))
+  (if (.exists (as-file file-name))
+    (load-file file-name)
+    (println "There is no solution yet for problem" number)))
+
+(defn -main
+  [& args]
+  (if (zero? (count args))
+    (println "Usage: run <problem number>")
+    (run-problem (read-string (first args)))))
