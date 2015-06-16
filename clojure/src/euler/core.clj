2a00032312e5231bdc05c5e35c8f321760a2b791
Rodrigo Navarro <rnavarro1@gmail.com>
2015-06-16 16:31:13 -0300

    Fixing code style for Clojure.

--- clojure/src/euler/core.clj
+++ clojure/src/euler/core.clj
@@ -2,15 +2,13 @@
 
 (use 'clojure.java.io)
 
-(defn run-problem
-  [number]
+(defn run-problem [number]
   (def file-name (str "src/euler/problem_" (format "%03d" number) ".clj"))
   (if (.exists (as-file file-name))
     (load-file file-name)
     (println "There is no solution yet for problem" number)))
 
-(defn -main
-  [& args]
+(defn -main [& args]
   (if (zero? (count args))
     (println "Usage: run <problem number>")
     (run-problem (read-string (first args)))))
