7c28cf657845fd9ccd361c24527f36cfab32b504
Rodrigo Navarro <rnavarro1@gmail.com>
2015-06-16 16:28:33 -0300

    Solution for problem 5 in Clojure.

---
+++ clojure/src/euler/problem_005.clj
@@ -0,0 +1,12 @@
+(defn greater-common-divisor [a b]
+  (if (zero? b)
+    a
+    (greater-common-divisor b (mod a b))))
+
+(defn lowest-common-multiple [a b]
+  (/ (* a b) (greater-common-divisor a b)))
+
+(def smallest-multiple
+  (reduce #(lowest-common-multiple %1 %2) (range 1 21)))
+
+(println smallest-multiple)
