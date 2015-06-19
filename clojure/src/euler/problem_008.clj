1f3d0fe208e0a95aeb209d4971116b7769661a0c
Rodrigo Navarro <rnavarro1@gmail.com>
2015-06-19 11:28:35 -0300

    Fixing typo.

--- clojure/src/euler/problem_008.clj
+++ clojure/src/euler/problem_008.clj
@@ -5,7 +5,7 @@
 (def digits
   (map #(bigint (str %)) (string/replace (slurp (resource "problem_008.txt")) #"\r?\n" "")))
 
-(defn largest-product-in-series [numbers size]
+(defn largest-product-in-a-series [numbers size]
   (reduce max (map #(reduce * %) (partition size 1 numbers))))
 
 (println (str (largest-product-in-a-series digits 13)))
